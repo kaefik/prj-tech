5b02cef670165f3847f3c3ab336c050b29acad0f
Ilnur Saifutdinov <ilnursoft@gmail.com>
2018-07-07 17:31:10 +0300

    done week3 js2

--- js2/week3/index.js
+++ js2/week3/index.js
@@ -15,20 +15,46 @@ ff = function (operations, callback) {
     }
 
     var result = [];
+    var errors =[];
 
-    next = function (err, data) {
-        if (err == null) {
-            result.push(data);
-            callback(null, result);
+    var count = operations.length;
+    var ferr = false;
+
+    //console.info(operations);
+
+    for (var index = 0; index < operations.length; index++) {
+        var operation = operations[index];
+        //console.info("i = ", index);
+        
+        foper = function (i) {
+            next = function (err, data) {
+                count--;
+                //console.info("count = ", count);
+                if (err == null) {
+                    result[i] = data;
+                    if (count == 0) {
+                        if (ferr) {
+
+                        } else {
+                            callback(null, result);   
+                        }
+                    } 
+                } else {
+                    ferr = true;
+                    callback(err, null);
+                    return;                   
+                }
+            }
+            var oper = operations[i];
+            oper(next);
         }
-        return err;
-    }
 
-    operations(next);
+        foper(index);
+    }
 };
 
 module.exports = ff;
-
+/*
 out = function(errors, result) {
     console.info("error = ", errors);
     console.info("result = ", result);
@@ -36,15 +62,60 @@ out = function(errors, result) {
 }
 
 
-//console.info(ff([], out));
+
+ff(      [
+    // Операция, которая выполняется 500ms
+    function (next) {
+        setTimeout(function () {
+            next(null, '500ms');
+        }, 500);
+
+    },
+
+    // Операция, которая выполняется 50ms
+    function (next) {
+        setTimeout(function () {
+            next(null, '50ms');
+        }, 50);
+    },
+
+    // Операция, которая выполняется 200ms
+    function (next) {
+        setTimeout(function () {
+            next(null, '200ms');
+        }, 200);
+    }
+],
+
+    // Обработка результата выполнения операций (результирующий callback)
+    out
+);
 
 
-ff(     // Операция, которая выполняется 500ms
+// Пример, когда одна из операций завершается ошибкой
+ff(
+    [
+        // Операция, которая выполняется 500ms
         function (next) {
             setTimeout(function () {
                 next(null, '500ms');
             }, 500);
-        }, 
-    // Обработка результата выполнения операций (результирующий callback)
+        },
+
+        // Операция, которая завершается с ошибкой через 10ms
+        function (next) {
+            setTimeout(function () {
+                next('ERROR');
+            }, 10);
+        },
+
+        // Операция, которая выполняется 200ms
+        function (next) {
+            setTimeout(function () {
+                next(null, '200ms');
+            }, 200);
+        }
+    ],
     out
 );
+*/
